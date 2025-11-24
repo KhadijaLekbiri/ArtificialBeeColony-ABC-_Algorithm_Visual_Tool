eeac4695f529b66f2f365f2d113b68271d8b0ec8
KhadijaLekbiri <lekbirikhadija3@gmail.com>
2025-11-24 11:12:46 +0100

    First commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.abc_algorithm {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.abc_algorithm to javafx.fxml;
+    exports com.example.abc_algorithm;
+}
